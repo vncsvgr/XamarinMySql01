26fcfcd343eb0257716620f78b53ba3d0535dabc
vncsvgr <vinicius.gabriel83@yahoo.com>
2022-04-27 22:34:21 -0300

    Adicionar arquivos de projeto.

---
+++ MySql01/MySql01/MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using MySqlConnector;
+
+namespace MySql01
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConectar_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                string strConexao =
+                    "server = " + edtServidor.Text +
+                    ";port = " + edtPorta.Text +
+                    ";uid = " + edtUsuario.Text +
+                    ";pwd = " + edtSenha.Text +
+                    ";database = " + edtBanco.Text;
+
+                MySqlConnection conexao = new MySqlConnection(strConexao);
+                conexao.Open();
+                DisplayAlert("Conexão", "Conexão realizada com sucesso!", "OK");
+                conexao.Close();
+            }
+            catch (Exception ex)
+            {
+                DisplayAlert("Erro", ex.Message, "OK");
+            }
+        }
+    }
+}
